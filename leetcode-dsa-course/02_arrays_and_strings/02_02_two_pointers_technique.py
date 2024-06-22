23af6b4e81d9342fd32facc394258c7c0b9d574c
bganguly <gangulybikramjit@hotmail.com>
2024-06-21 20:35:01 -0700

    update 02_02_two_pointers

--- leetcode-dsa-course/02_arrays_and_strings/02_02_two_pointers_technique.py
+++ leetcode-dsa-course/02_arrays_and_strings/02_02_two_pointers_technique.py
@@ -11,5 +11,58 @@ def isPalindrome (input):
   return True  
 
 
-print(isPalindrome('abaaba'))
+# print(isPalindrome('abaaba'))
 
+def does_pairwise_sum_exist (nums, target):
+  left = 0
+  right = len(nums) - 1
+
+  while left < right:
+    curr = nums[left] + nums[right]
+    if curr == target:
+        return True
+    if curr > target:
+        right -= 1
+    else:
+        left += 1
+    
+  return False
+
+
+# print(does_pairwise_sum_exist([1, 2, 4, 6, 8, 9, 14, 15], 13))
+
+def combine_sorted_arrays(arr1, arr2):
+    # ans is the answer
+    ans = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+    
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+    
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+    
+    return ans
+
+# print(combine_sorted_arrays([1,4,7,20], [3,5,6]))
+
+def isSubsequence(s: str, t: str) -> bool:
+    i = j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+
+    return i == len(s)
+
+
+print(isSubsequence("tig", "tiger"))
