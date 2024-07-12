e5b797f873b576027d5373a733722c3c5ca3377b
bganguly <gangulybikramjit@hotmail.com>
2024-07-12 12:59:56 -0700

    update section 05 - project 1

--- udemy-dsa/05_project_01_arrays_lists/05_project_01_array_list.py
+++ udemy-dsa/05_project_01_arrays_lists/05_project_01_array_list.py
@@ -1,12 +1,17 @@
-numdays = int(input('how many days: '))
-print ('Enter highest temperature for each day as integer - no decimals')
-temp = []
-for i in range(numdays):
-    nextDay = int(input('Day ' + str(i+1) + ' high temp: '))
-    temp.append(nextDay)
-avgTemp = sum(temp)/numdays
-print ()
-print ('** printing statistics below ** ')
-print ('avg temperarture is : ', avgTemp)
-high_temp = [ i for i in temp if i > avgTemp ]    
-print('number of days higher than avg temperarture is : ', len(high_temp))
+def generate_temp_statistics():
+
+  numdays = int(input('how many days: '))
+  print ('Enter highest temperature for each day as integer - no decimals')
+  temp = []
+  for i in range(numdays):
+      nextDay = int(input('Day ' + str(i+1) + ' high temp: '))
+      temp.append(nextDay)
+  avgTemp = sum(temp)/numdays
+  print ()
+  print ('** printing statistics below ** ')
+  print ('avg temperarture is : ', avgTemp)
+  high_temp = [ i for i in temp if i > avgTemp ]    
+  print('number of days higher than avg temperarture is : ', len(high_temp))
+
+
+generate_temp_statistics()
