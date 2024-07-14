cd2e7083c230c1af096da92e5d1e625087ed4787
bganguly <gangulybikramjit@hotmail.com>
2024-07-14 15:26:45 -0700

    update section 06- codding exercise 1- question #2- extracted out eror conditions

--- udemy-dsa/06_arrays_lists_coding_exercises/06_01_missing_number.py
+++ udemy-dsa/06_arrays_lists_coding_exercises/06_01_missing_number.py
@@ -1,4 +1,7 @@
-# coding exercise 1: 
+# coding exercise 1:
+
+import numbers
+import numpy as np
 
 # question 1
 # missing number
@@ -12,10 +15,10 @@ def missing_number(arr, n):
     sum_of_elements_in_arr = sum(arr)
     return sum_of_n_numbers - sum_of_elements_in_arr
 
-print(missing_number([1, 2, 3, 4, 6], 6))
+print(f"Missing number is: {missing_number([1, 2, 3, 4, 6], 6)}")
 
 # question 2
-# pairs- two sum: ;eetcode #1
+# pairs- two sum: leetcode #1
 # find indices of numbers (in an array) that add to a target number
 # Write a function to find the indices of elements whose sum add to a given array.
 # Example
@@ -27,7 +30,7 @@ def two_sum(nums, target):
         if complement in seen:
             return [seen[complement], i]
         seen[num] = i
- 
+
 nums = [2, 7, 11, 15]
 target = 26
 indices = two_sum(nums, target)
@@ -36,32 +39,42 @@ print()
 
 # question 3
 # check if element exist in array
-import numpy as np
-import numbers
 def find(array, value):
-    """ Returns a tuple is_error, value_found, index_found """
-    if type(array) == np.ndarray and array.size > 1:
+  """ Returns a tuple is_error, input_is_not_array, value_is_not_number, value_found, index_found """
+  if type(array) == np.ndarray and array.size > 1:
+    if isinstance(value, numbers.Number):
       for i, num in enumerate(array):
           if num == value:
-              return False, value, i
-      return True, value    
-    else:  
-      return True
+              return False, False, False, value, i
+      return True, False, False, value, None
+    else:
+      return True, False, True, value, None
+  else:
+    if isinstance(value, numbers.Number):
+      return True, True, False, value, None
+    else:
+      return True, True, True, value, None
 
 def find_helper(result):
-  if isinstance(result, tuple):
-    if result[0] == False:
-      print(f'element {result[1]} found at index: {result[2]}')
-    elif isinstance(result[1], numbers.Number) == True:  
-        print(f'element {result[1]} not found in array')
-  else:  
-    print(f'first argument is not a np.array')
+  if result[0] == False:
+    print(f'value {result[3]} found at index: {result[4]}')
+  else:
+    if result[1] == False:
+      if result[2] == False:
+        print(f'value {result[3]} not found in array')
+      else:
+        print(f'value {result[3]} is not numeric')
+    else:
+      if result[2] == False:
+        print('input array is not a np.array')
+      else:
+        print('input array is not a np.array and value is also not numeric')
 
-result = find(np.array([1,2,3]), 3)
+result = find(np.array([1, 2, 3]), 3)
 find_helper(result)
-result = find(np.array([1,2,3]), -3)
+result = find(np.array([1, 2, 3]), -3)
 find_helper(result)
-result = find(np.array([1,2,3]), None)
+result = find(np.array([1, 2, 3]), None)
 find_helper(result)
 result = find(np.array(None), None)
 find_helper(result)
