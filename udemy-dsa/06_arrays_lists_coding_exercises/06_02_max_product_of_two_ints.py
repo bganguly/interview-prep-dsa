bbc082f2d5b5c1ac35977e22f2ac96ad22637e6f
bganguly <gangulybikramjit@hotmail.com>
2025-01-11 19:08:13 -0800

    add leetcode top k frequent elements own solution in js

--- udemy-dsa/06_arrays_lists_coding_exercises/06_02_max_product_of_two_ints.py
+++ udemy-dsa/06_arrays_lists_coding_exercises/06_02_max_product_of_two_ints.py
@@ -1,12 +1,12 @@
 # coding exercise 2:
 # max product of two integers
-def max_product(arr):
+def max_product_unoptimised(arr):
     arr.sort(reverse=False)
     return arr[len(arr)-1] * arr[len(arr)-2]
     # arr.sort(reverse=True)
     # return arr[0]*arr[1]
 arr = [1, 7, 3, 4, 9, 5] 
-print(max_product(arr)) # Output: 63 (9*7)
+print(max_product_unoptimised(arr)) # Output: 63 (9*7)
 
 # - since the sort above would be NlogN
 # better way below - O(n) in time and O(1) in space 
